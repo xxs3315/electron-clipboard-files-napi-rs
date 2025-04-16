37836e2b976e3b79f6d99edf6c5c5257bade5c32
xxs3315 <xxs3315@foxmail.com>
2025-04-16 17:07:09 +0800

    feat: add getText, getRtf, getHtml

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import { hasFiles, getFiles, hasHtml, hasImage, hasRtf, hasText, getText, getHtml, getRtf } from '../index'
+import { hasFiles, getFiles, hasHtml, hasImage, hasRtf, hasText } from '../index'
 
 test('has files', (t) => {
   t.is(typeof hasFiles(), 'boolean')
@@ -24,16 +24,4 @@ test('has text', (t) => {
 
 test('get files', (t) => {
   t.is(typeof getFiles(), 'object')
-})
-
-test('get text', (t) => {
-  t.is(typeof getText(), 'string')
-})
-
-test('get rtf', (t) => {
-  t.is(typeof getRtf(), 'string')
-})
-
-test('get html', (t) => {
-  t.is(typeof getHtml(), 'string')
 })
